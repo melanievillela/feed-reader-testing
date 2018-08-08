377ae5f4b156f4976cb654b6ef0aa33645fde474
Melanie Villela <melanienvillela@gmail.com>
2018-08-07 20:22:23 -0500

    Update
    Fixed tests per Udacity review feedback.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -59,14 +59,9 @@ $(function() {
         });
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function() {
-      /* TODO: Write a test that ensures when the loadFeed
-       * function is called and completes its work, there is at least
-       * a single .entry element within the .feed container.
-       * Remember, loadFeed() is asynchronous so this test will require
-       * the use of Jasmine's beforeEach and asynchronous done() function.
-       */
+
+       //Check that single .entry element in .feed container
        let container = $('.feed');
 
        beforeEach(function(done) {
@@ -75,20 +70,17 @@ $(function() {
        });
 
        it("Contains entry", function() {
-         expect(container.length).not.toBe(0);
-         //expect(container[0].firstElementChild.children[0].hasClass("entry"))
+         expect(container.children.length).not.toBe(0);
        });
      });
 
-     /* TODO: Write a new test suite named "New Feed Selection" */
      describe('New Feed Selection', function() {
-       /* TODO: Write a test that ensures when a new feed is loaded
-        * by the loadFeed function that the content actually changes.
-        * Remember, loadFeed() is asynchronous.
-        */
+
+        //Check new feed is loaded and that content changes
         let container = $('.feed');
         let x;
         let y;
+
         beforeEach(function(done) {
           loadFeed(0, function() {
             x = container["0"].children["0"].innerText;
